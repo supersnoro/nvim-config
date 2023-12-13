7ce4be165e3c8764f89b08008f325d7e6b7bc951
Charles Étienne Lalonde <charles-Etienne.lalonde@ubisoft.com>
2023-12-13 15:41:56 -0500

    fix: disable debug

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -9,6 +9,7 @@
 return {
   -- NOTE: Yes, you can install new plugins here!
   'mfussenegger/nvim-dap',
+  cond = not vim.g.vscode,
   -- NOTE: And you can specify dependencies as well
   dependencies = {
     -- Creates a beautiful debugger UI
