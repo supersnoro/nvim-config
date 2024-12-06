47e8e94ef2567b4039671c0b77f19e9f5d1affc4
super snoro <supersnoro@gmail.com>
2024-12-06 01:45:34 -0500

    Attempts to fix LSP not populating things properly

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -88,7 +88,40 @@ return {
 					end,
 				},
 				completion = { completeopt = 'menu,menuone,noinsert' },
-				mapping = cmp_keymaps.insert(),
+				mapping = {
+					['<C-n>'] = cmp.mapping.select_next_item(),
+					['<C-p>'] = cmp.mapping.select_prev_item(),
+
+					['<C-d>'] = cmp.mapping.scroll_docs(-4),
+					['<C-f>'] = cmp.mapping.scroll_docs(4),
+
+					['<C-y>'] = cmp.mapping.complete { select = true },
+
+					-- Manually trigger a completion from nvim-cmp.
+					--  Generally you don't need this, because nvim-cmp will display
+					--  completions whenever it has completion options available.
+					['<C-Space>'] = cmp.mapping.complete {},
+
+					-- Think of <c-l> as moving to the right of your snippet expansion.
+					--  So if you have a snippet that's like:
+					--  function $name($args)
+					--    $body
+					--  end
+					--
+					-- <c-l> will move you to the right of each of the expansion locations.
+					-- <c-h> is similar, except moving you backwards.
+					['<C-l>'] = cmp.mapping(function()
+						if luasnip.expand_or_locally_jumpable() then
+							luasnip.expand_or_jump()
+						end
+					end, { 'i', 's' }),
+					['<C-h>'] = cmp.mapping(function()
+						if luasnip.locally_jumpable(-1) then
+							luasnip.jump(-1)
+						end
+					end, { 'i', 's' }),
+
+				},
 				sources = {
 					{
 						name = 'lazydev',
